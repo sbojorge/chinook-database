6e3e6598538aa33091c3b88774378db80a8c6f67
sbojorge <sara.bojorge@gmail.com>
2023-02-22 16:19:01 +0000

    Copy and paste the entire source code but the ORM method still have an error

--- sql-crud.py
+++ sql-crud.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-# executing the instructions for the chinook database
+# executing the instructions from the "chinook" database
 db = create_engine("postgresql:///chinook")
 base = declarative_base()
 
@@ -21,17 +21,17 @@ class Programmer(base):
     famous_for = Column(String)
 
 
-# instead of connecting to the database directly we will ask a session
+# instead of connecting to the database directly, we will ask for a session
 # create a new instance of sessionmaker, then point to our engine (the db)
 Session = sessionmaker(db)
-# opens the actual session by calling the Session() subclass define above
+# opens an actual session by calling the Session() subclass defined above
 session = Session()
 
 # creating the database using declarative_base subclass
 base.metadata.create_all(db)
 
 
-# creating records in our Programmer table
+# creating records on our Progammer table
 ada_lovelace = Programmer(
     first_name="Ada",
     last_name="Lovelace",
@@ -40,13 +40,59 @@ ada_lovelace = Programmer(
     famous_for="First Programmer"
 )
 
+alan_turing = Programmer(
+    first_name="Alan",
+    last_name="Turing",
+    gender="M",
+    nationality="British",
+    famous_for="Modern Computing"
+)
+
+grace_hopper = Programmer(
+    first_name="Grace",
+    last_name="Hopper",
+    gender="F",
+    nationality="American",
+    famous_for="COBOL language"
+)
+
+margaret_hamilton = Programmer(
+    first_name="Margaret",
+    last_name="Hamilton",
+    gender="F",
+    nationality="American",
+    famous_for="Apollo 11"
+)
+
+bill_gates = Programmer(
+    first_name="Bill",
+    last_name="Gates",
+    gender="M",
+    nationality="American",
+    famous_for="Microsoft"
+)
+
+tim_berners_lee = Programmer(
+    first_name="Tim",
+    last_name="Berners-Lee",
+    gender="M",
+    nationality="British",
+    famous_for="World Wide Web"
+)
+
 # add each instance of our programmers to our session
 session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
 
 # commit our session to the database
-session.commit()
+# session.commit()
 
-# query the database to find all programmers
+
+# query the database to find all Programmers
 programmers = session.query(Programmer)
 for programmer in programmers:
     print(
